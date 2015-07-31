59fddb6fabadc58ccb5672d023b57afcb55abaea
choijaewoong <jwchoi0129@gmail.com>
2015-08-01 01:03:45 +0900

    Update LetterCapitalize.rb

--- LetterCapitalize.rb
+++ LetterCapitalize.rb
@@ -1,15 +1,14 @@
 def LetterCapitalize(str)
-
-  # code goes here
+  
   result = String.new
   
   str.split.each do |x|
-    
     result += x.capitalize    
     if x != str.split.last
       result += " " 
     end   
   end 
+  
   return result         
 end
    
